fe3c4078203569e4dd32c3a6752a92a4fdde3990
Peter Garside-Quin <petegq@gmail.com>
2021-11-03 15:23:56 +0000

    Initialise slice and append to it

--- cards/main.go
+++ cards/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Two of Clubs")
+	for i := 0; i < len(cards); i++ {
+		fmt.Println(cards[i])
+	}
 }
 
 func newCard() string {
-	return "Five of Diamonds"
+	return "Five of Spades"
 }
