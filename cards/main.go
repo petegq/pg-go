3dfc322f73736ff6cc8bd7b0a4bcb08c199b6fa6
Peter Garside-Quin <petegq@gmail.com>
2021-11-03 13:27:17 +0000

    More vars

--- cards/main.go
+++ cards/main.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
+var card string
+
 func main() {
+	// var card string
 	// var card string = "Ace of Spades"
-	card := "Ace of Clubs"
+	// card := "Ace of Clubs"
 	card = "Five of Spades"
 
 	fmt.Println(card)
